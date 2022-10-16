58f7b35893257600d4e09c46d3254c799a3fad30
becca <93043262+beccaric@users.noreply.github.com>
2022-10-16 13:36:29 +0000

    HLT 2

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ def totalbill():
 
 print(totalbill())
 
+#HLT2
+#Write a program that will ask for a number of days and will then show how many hours, minutes and seconds are in that number of days 
+
+days=int(input("Please enter the number of days:"))
+hours = days*24
+minutes = hours*60
+seconds = minutes*60
+
+print("In", days, "days, there are")
+print(hours, "hours")
+print(minutes, "minutes")
+print(seconds, "seconds")
+
 #HLT3
 #Ask the user’s age. If they are 18 or over, display the message “You can vote”. If they are aged 17, display the message “You can learn to drive”. If they are 16, display the message, “You can buy a lottery ticket”. If they are under 16, display the message “You can go trick or treating” 
 
